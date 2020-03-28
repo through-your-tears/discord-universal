dcd53a304d285e24c653f292820d3bf599e50545
through-your-tears <62816146+through-your-tears@users.noreply.github.com>
2020-03-29 01:07:42 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import random
+import discord
+import os
+from discord.ext import commands
+TOKEN='Njg5NTgzMDY3NTA5NDI0MjEz.XnE-aQ.WShucaO0bkOS3qmpbpwyelyy5GI'
+bot = commands.Bot(command_prefix='!')
+d={}
+@bot.command(pass_context=True)
+async def hi(ctx):
+    await ctx.send('Hey, {}'.format(ctx.message.author.mention))
+@bot.command(pass_context=True)
+async def dicklen(ctx):
+    try:
+        await ctx.send(d[format(ctx.message.author.id)] + 'cm - is your dick size {}'.format(ctx.message.author.mention))
+    except KeyError:
+        c={format(ctx.message.author.id): str(random.randrange(10, 31))}
+        d.update(c)
+        await ctx.send(d[format(ctx.message.author.id)] + 'cm - is your dick size {}'.format(ctx.message.author.mention))
+#bot.run(TOKEN)
+token - os.environ.get('BOT_TOKEN')
