5fe490d69cc8825f68ff45d2bd2456636b1ed208
through-your-tears <62816146+through-your-tears@users.noreply.github.com>
2020-04-02 01:36:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,19 @@ import discord
 import os
 from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 d={}
+@bot.event
+async def on_ready():
+    await bot.change_presence( status = discord.Status.online, activity = discord.Game('Личный клоун Дмитрия'))
 @bot.command(pass_context=True)
+async def help(ctx):
+    emb=discord.Embed(title='Список возможных команд')
+    emb.add_field(name = '{}hi'.format('!'), value = 'Поздороваться')
+    emb.add_field(name = '{}dicklen'.format('!'), value = 'Измерить длину вашего прибора')
+    emb.add_field(name = '{}clear (введенное число)'.format('!'), value = 'Удалить последние (введенное число) сообщений')
+    await ctx.send(embed = emb)
+@bot.command(pass_context=True)ф
 async def hi(ctx):
     await ctx.send('Hey, {}'.format(ctx.message.author.mention))
 @bot.command(pass_context=True)
